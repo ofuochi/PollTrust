6b362596e8eb883b64eafb8788b9a151860319f1
ofuochi <fortuneochi@gmail.com>
2017-07-05 11:57:22 +0100

    Fix to options validation

--- modules/polls/client/controllers/polls.client.controller.js
+++ modules/polls/client/controllers/polls.client.controller.js
@@ -2,98 +2,97 @@
 
 // Polls controller
 angular.module('polls').controller('PollsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Polls',
-  function ($scope, $stateParams, $location, Authentication, Polls) {
-    $scope.authentication = Authentication;
-    $scope.options = [{text:null},{text:null}];
+    function($scope, $stateParams, $location, Authentication, Polls) {
+        $scope.authentication = Authentication;
+        $scope.options = [{ text: null }, { text: null }];
 
-    //Add an option
-     $scope.addOption = function() {
-      $scope.options.push({
-        text:null
-      });
-    };
+        //Add an option
+        $scope.addOption = function() {
+            $scope.options.push({
+                text: null
+            });
+        };
 
-    //Remove an option
-    $scope.removeOption = function(id){
-      $scope.poll.options.splice(id, 1);
-    };
+        //Remove an option
+        $scope.removeOption = function(id) {
+            $scope.poll.options.splice(id, 1);
+        };
 
-    // Create new Poll
-    $scope.create = function (isValid) {
-      $scope.error = null;
+        // Create new Poll
+        $scope.create = function(isValid) {
+            $scope.error = null;
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'pollForm');
+            if (!isValid) {
+                $scope.$broadcast('show-errors-check-validity', 'pollForm');
 
-        return false;
-      }
+                return false;
+            }
 
-      // Create new Poll object
-      var poll = new Polls({
-        title: this.title,
-        content:this.content,
-        options: [{text:null}]
-      });
-      console.log(poll);
-      // Redirect after save
-      poll.$save(function (response) {
-        $location.path('polls/' + response._id);
+            // Create new Poll object
+            var poll = new Polls({
+                title: this.title,
+                options: [{ text: '3' }, { text: 87 }]
+            });
+            console.log(poll);
+            // Redirect after save
+            poll.$save(function(response) {
+                $location.path('polls/' + response._id);
 
-        // Clear form fields
-        $scope.title = '';
-        // $scope.content = '';
-        $scope.options = [{text:"",id:1},{text:"",id:2}];
-      }, function (errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
-    };
+                // Clear form fields
+                $scope.title = '';
+                // $scope.content = '';
+                $scope.options = [{ text: "", id: 1 }, { text: "", id: 2 }];
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-    // Remove existing Poll
-    $scope.remove = function (poll) {
-      if (poll) {
-        poll.$remove();
+        // Remove existing Poll
+        $scope.remove = function(poll) {
+            if (poll) {
+                poll.$remove();
 
-        for (var i in $scope.polls) {
-          if ($scope.polls[i] === poll) {
-            $scope.polls.splice(i, 1);
-          }
-        }
-      } else {
-        $scope.poll.$remove(function () {
-          $location.path('polls');
-        });
-      }
-    };
+                for (var i in $scope.polls) {
+                    if ($scope.polls[i] === poll) {
+                        $scope.polls.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.poll.$remove(function() {
+                    $location.path('polls');
+                });
+            }
+        };
 
-    // Update existing Poll
-    $scope.update = function (isValid) {
-      $scope.error = null;
+        // Update existing Poll
+        $scope.update = function(isValid) {
+            $scope.error = null;
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'pollForm');
+            if (!isValid) {
+                $scope.$broadcast('show-errors-check-validity', 'pollForm');
 
-        return false;
-      }
+                return false;
+            }
 
-      var poll = $scope.poll;
+            var poll = $scope.poll;
 
-      poll.$update(function () {
-        $location.path('polls/' + poll._id);
-      }, function (errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
-    };
+            poll.$update(function() {
+                $location.path('polls/' + poll._id);
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-    // Find a list of Polls
-    $scope.find = function () {
-      $scope.polls = Polls.query();
-    };
+        // Find a list of Polls
+        $scope.find = function() {
+            $scope.polls = Polls.query();
+        };
 
-    // Find existing Poll
-    $scope.findOne = function () {
-      $scope.poll = Polls.get({
-        pollId: $stateParams.pollId
-      });
-    };
-  }
+        // Find existing Poll
+        $scope.findOne = function() {
+            $scope.poll = Polls.get({
+                pollId: $stateParams.pollId
+            });
+        };
+    }
 ]);
--- modules/polls/server/models/poll.server.model.js
+++ modules/polls/server/models/poll.server.model.js
@@ -1,54 +1,51 @@
-'use strict';
+"use strict";
 
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+var mongoose = require("mongoose"),
+    Schema = mongoose.Schema;
 
 /**Option Schema */
 var OptionSchema = new Schema({
-  text:{
-    type:String,
-    default:'',
-    trim:true,
-    required:'All options must be filled'
-  }
+    text: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'All options must be filled'
+    }
 });
 
 /**
  * Poll Schema
  */
 var PollSchema = new Schema({
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  title: {
-    type: String,
-    default: '',
-    trim: true,
-    required: 'Title cannot be blank'
-  },
-  // content: {
-  //   type: String,
-  //   default: '',
-  //   trim: true,
-  //   required: "Content cannot be blank"
-  // },
-  options:{
-     type: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Option'
-    }],
-    validate: [arrayLimit, '{PATH} must be at least two options']
-  },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        type: String,
+        default: "",
+        trim: true,
+        required: "Title cannot be blank"
+    },
+    // content: {
+    //   type: String,
+    //   default: '',
+    //   trim: true,
+    //   required: "Content cannot be blank"
+    // },
+    options: {
+        type: [OptionSchema]
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: "User"
+    }
 });
-function arrayLimit(val) {
-  return val.length > 2;
-}
-mongoose.model('Poll', PollSchema);
+
+PollSchema.path('options').validate(function(value) {
+    return value.length >= 2;
+}, "At least two options are required");
+mongoose.model("Poll", PollSchema);
